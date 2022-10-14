18fa633f4d89ad1061704ab43578c73811a80d4b
max7394 <mlysenko92@gmail.com>
2022-10-14 14:52:31 +0300

    feat: add solve stack-task

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor () {
+    this.array = [];
+  }
+
+  push(element) {
+    return this.array.push(element);
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    return this.array.pop();
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.array[this.array.length - 1]
     // remove line with error and write your code here
   }
 }
